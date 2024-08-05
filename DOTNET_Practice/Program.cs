e9b59df3ec39272fbf7b99f8fadd32dd674ae04c
Hasti Hajipara <hasti@EVISION.COM>
2024-08-05 17:41:19 +0530

    Struct and Enum

--- DOTNET_Practice/Program.cs
+++ DOTNET_Practice/Program.cs
@@ -4,6 +4,7 @@ namespace Program
 {
     class Program
     {
+        //properties Example
         private double _num;
         public double num
         {
@@ -23,6 +24,20 @@ namespace Program
                 }
             }
         }
+
+        // Struct demo
+        struct Person
+        {
+            public string name;
+            public int age;
+            public int id;
+        }
+
+        //enum
+        public enum Product
+        {
+            Milk = 0, Juice = 1, Tea = 2
+        }
         static void Main(string[] args)
         {
 
@@ -36,13 +51,30 @@ namespace Program
             DestructorDemo obj1 = new DestructorDemo();
             DestructorDemo obj2 = new DestructorDemo();
             // Making obj1 for Garbage Collection
-             obj1 = null;
-             GC.Collect();
+            obj1 = null;
+            GC.Collect();
 
             Program p = new Program();
             p._num = 10;
 
             Console.WriteLine("Hello World");
+
+            Person Person1;
+            Person1.age = 20;
+            Person1.name = "Test";
+            Person1.id = 10;
+
+            Console.WriteLine("Age : " + Person1.age);
+
+            // enum 
+            var test1 = Product.Tea;
+            var test2 = Product.Milk;
+            Console.WriteLine((int)test1 + " " + test2);
+
+            string test3 = "Tefa";
+            Product getProduct;
+            bool checkParse = Enum .TryParse(test3 , out getProduct);
+            Console.WriteLine(checkParse);
         }
     }
 }
