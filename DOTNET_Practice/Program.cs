af2055a222a7e0cc880919111a8a16069318e0de
Hasti Hajipara <hasti@EVISION.COM>
2024-08-05 12:32:19 +0530

    Singleton Pattern Example

--- DOTNET_Practice/Program.cs
+++ DOTNET_Practice/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using DOTNET_Practice;
+
+var singleton = SingletonDesignPattern.Instance(10);
+Console.WriteLine($"SIngleton Pattern Object is {singleton.Id}");
+
+var singleton2 = SingletonDesignPattern.Instance(20);
+Console.WriteLine($"SIngleton Pattern Object is {singleton2.Id}");
--- DOTNET_Practice/SingletonDesignPattern.cs
+++ DOTNET_Practice/SingletonDesignPattern.cs
@@ -6,11 +6,28 @@ using System.Threading.Tasks;
 
 namespace DOTNET_Practice
 {
-    internal class SingletonDesignPattern
+    sealed class SingletonDesignPattern
     {
-        private SingletonDesignPattern()
+        public int Id { get; private set; }
+        private SingletonDesignPattern(int id)
         {
-            
+            this.Id = id;
+        }
+        private static SingletonDesignPattern _instance;
+        private static readonly object _lock = new object();
+        public static SingletonDesignPattern Instance(int id)
+        {
+            if (_instance == null)
+            {
+                lock(_lock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new SingletonDesignPattern(id);
+                    }
+                }
+            }
+            return _instance;
         }
     }
 }
