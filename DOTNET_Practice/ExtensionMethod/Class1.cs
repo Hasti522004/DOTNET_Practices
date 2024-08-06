b6078861e1f0292f7539630d9465f5f800d992b8
Hasti Hajipara <hasti@EVISION.COM>
2024-08-06 14:17:36 +0530

    Extension methods in c#

---
+++ DOTNET_Practice/ExtensionMethod/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET_Practice.ExtensionMethod
+{
+    // Extension Method must be define in static class
+    static class Class1
+    {
+        public static int GetDigits(this int num)
+        {
+            int count = 0;
+            while(num!=0)
+            {
+                count++;
+                num = num/10;
+            }
+            return count;
+        }
+        public static int MyString(this string name)
+        {
+            return 10;
+        }
+    }
+}
---
+++ DOTNET_Practice/ExtensionMethod/ExtensionMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DOTNET_Practice.ExtensionMethod;
+
+namespace ExtenstionMethodDemo
+{
+    class ExtensionMethod
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            int k = a.GetDigits();
+            Console.WriteLine(k);
+
+            string name = "Hasti";
+            int j = name.MyString();
+            Console.WriteLine(j);
+        }
+    }
+}
