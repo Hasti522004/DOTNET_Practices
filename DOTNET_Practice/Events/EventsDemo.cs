747a6d0222fc5391e67e838647ee0fdbc40b4755
Hasti Hajipara <hasti@EVISION.COM>
2024-08-06 15:54:32 +0530

    Delegate and Events in c#

---
+++ DOTNET_Practice/Events/EventsDemo.cs
@@ -0,0 +1,22 @@
+﻿namespace EventsDemo
+{
+    delegate string Mydel(string s);
+    class EventsDemo
+    {
+        event Mydel Myevent;
+        public EventsDemo()
+        {
+            this.Myevent += new Mydel(this.Mymethod);
+        }
+        public string Mymethod(string s)
+        {
+            return "Welcome " + s;
+        }
+        public static void Main(string[] args)
+        {
+            EventsDemo demo = new EventsDemo();
+            string mystr = demo.Myevent("Hasti");
+            Console.WriteLine(mystr);
+        }
+    }
+}
