afa270faacc0e19c6340d89172c9e81ff3f55e9c
Hasti Hajipara <hasti@EVISION.COM>
2024-08-06 11:23:23 +0530

    Ways to call Methods in C#

---
+++ DOTNET_Practice/methodCallingDemo/MethodCallingDemo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET_Practice.methodCallingDemo
+{
+    class MethodCallingDemo
+    {
+        static void Method1(ref int x)
+        {
+            Console.WriteLine(x);
+            x = 100;
+            Console.WriteLine(x);
+
+        }
+
+        static void Method2(int x)
+        {
+
+        }
+
+        static void Method3(out int x)
+        {
+            x = 23;
+        }
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("from Method Calling Demo");
+            int a = 10;
+            Console.WriteLine(a);
+            Method2(a); // call by value
+            Method1(ref a); // call by reference
+            Method3(out a);
+            Console.WriteLine(a);
+        }
+    }
+}
