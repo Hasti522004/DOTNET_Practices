9ee96cdcdba8c746e2bde5f747c42ea5e46bf58b
Hasti Hajipara <hasti@EVISION.COM>
2024-08-07 17:56:17 +0530

    implement new collections in C#

--- DOTNET_Practice/CollectionDemo/CollectionDemo.cs
+++ DOTNET_Practice/CollectionDemo/CollectionDemo.cs
@@ -42,6 +42,37 @@ namespace DOTNET_Practice.CollectionDemo
                 Console.WriteLine(s); 
             }
 
+            // 2D array
+            int[,] arr = new int[3,3];
+            arr[0, 0] = 1; arr[1,0] = 2; arr[2,0]   = 3;
+            arr[0,1] = 4; arr[1,1] = 5; arr[2, 1] = 6;
+            Console.WriteLine(arr[2,1]);
+
+            // Jagged Array 
+            int[][] jaggedarray = new int[3][];
+            jaggedarray[0] = new int[5];
+            jaggedarray[1] = new int[2];
+            jaggedarray[2] = new int[6];
+
+            int num = 0;
+            for(int i = 0; i < jaggedarray.GetLength(0); i++)
+            {
+                for(int j = 0;j < jaggedarray[i].Length; j++)
+                {
+                    num++;
+                    jaggedarray[i][j] = num;
+                }
+            }
+
+            for (int i = 0; i < jaggedarray.GetLength(0); i++)
+            {
+                for (int j = 0; j < jaggedarray[i].Length; j++)
+                {
+                    Console.Write($"{jaggedarray[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+
             // Non - generic Collections
             // 3. ArrayList
             Console.WriteLine("\nArrayList : ");
@@ -55,10 +86,15 @@ namespace DOTNET_Practice.CollectionDemo
             arrList.Insert(1, "1st Index");
             arrList.Remove(1);
             // arrList.Clear();
+
             foreach( var s in arrList)
             {
                 Console.WriteLine(s); ;
             }
+
+            int totalItems = arrList.Count;
+            Console.Write("Total items is : ");
+            Console.WriteLine(totalItems);
 
             // 4. HashTable
             Console.WriteLine("\nHashTable");
@@ -72,6 +108,23 @@ namespace DOTNET_Practice.CollectionDemo
             }
             Console.WriteLine(ht["1"]);
             Console.WriteLine(ht.Contains(1));
+
+            //5. Stack
+            Console.WriteLine("\nStack Implementation : ");
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push("Hasti");
+            stack.Push(true);
+            stack.Push(1.22f);
+            stack.Pop();
+            var temp1 = stack.IsSynchronized;
+            Console.WriteLine($"temp1 {temp1}");
+            var temp = stack.Peek();
+            Console.WriteLine($"peek from stack {temp}");
+            foreach (object o in stack)
+            {
+                Console.WriteLine(o);
+            }
 
             // Generic Collections 
             // 1. List
