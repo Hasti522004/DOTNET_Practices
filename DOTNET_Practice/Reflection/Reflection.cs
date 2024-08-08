b7b1d2539da064737c3a4c71c239b68d414ef988
Hasti522004 <hasti.hajipara@gmail.com>
2024-08-08 17:44:01 +0530

    Reflection in c#

---
+++ DOTNET_Practice/Reflection/Reflection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET_Practice.Reflection
+{
+    internal class Reflection
+    {
+        public static void Main(string[] args)
+        {
+            Type s = typeof(int);
+            Console.WriteLine(s.Name);
+            Console.WriteLine(s.FullName);
+            Console.WriteLine(s.BaseType);
+            Console.WriteLine(s.Namespace);
+        }
+    }
+}
