bcb1389436b186289487ea90820d1e256c5ee97b
Hasti522004 <hasti.hajipara@gmail.com>
2024-08-08 16:05:06 +0530

    Dependency Injection More Example in c#

---
+++ DOTNET_Practice/DependencyInjection3/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET_Practice.Testing
+{
+    interface ILogging
+    {
+        void Log();
+    }
+    class FileLogging : ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("File Logging");
+        }
+    }
+    class ConsoleLogging : ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Console Logging");
+        }
+    }
+    class LoggerUser
+    {
+        private ILogging _log;
+        public LoggerUser(ILogging ilog)
+        {
+            _log = ilog;
+        }
+        public void Display()
+        {
+            _log.Log();
+        }
+    }
+    internal class Class1
+    {
+        public static void Main(string[] args)
+        {
+            ILogging logging = new ConsoleLogging();
+            LoggerUser user = new LoggerUser(logging);
+            user.Display(); 
+        }
+    }
+}
