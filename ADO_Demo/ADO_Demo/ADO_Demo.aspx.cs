9a527d8b0be66238a70db2f74be553b3a3cc39b8
Hasti522004 <hasti.hajipara@gmail.com>
2024-08-13 18:11:08 +0530

    ADO database Connection with web.config file

--- ADO_Demo/ADO_Demo/ADO_Demo.aspx.cs
+++ ADO_Demo/ADO_Demo/ADO_Demo.aspx.cs
@@ -5,20 +5,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Configuration;
 namespace ADO_Demo
 {
     public partial class ADO_Demo : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("Data Source=EV-LAP-00158\\MSSQLSERVEV;password=Welcome@123;Initial Catalog=student;Persist Security Info=True;User ID=sa;Encrypt=True;TrustServerCertificate=true");
-            SqlCommand cmd = new SqlCommand("select * from hr.candidates", con);
-            con.Open();
-            SqlDataReader reader = cmd.ExecuteReader();
-            GridView1.DataSource = reader;
-            GridView1.DataBind();
-            con.Close();
+            try
+            {
+                // Retrieve the connection string from web.config
+                string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+                using (SqlConnection con = new SqlConnection(CS))
+                {
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM product_json;", con);
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    GridView1.DataSource = reader;
+                    GridView1.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log or display the exception
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
         }
+
+
     }
 }
