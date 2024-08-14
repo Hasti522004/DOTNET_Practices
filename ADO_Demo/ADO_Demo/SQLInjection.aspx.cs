c8c76f2a6b791d1fc34393c35cd6cedfa0d4aa40
Hasti522004 <hasti.hajipara@gmail.com>
2024-08-14 11:13:46 +0530

    SQL Injection Prevension using Parameterized Query and Store Procedures

--- ADO_Demo/ADO_Demo/SQLInjection.aspx.cs
+++ ADO_Demo/ADO_Demo/SQLInjection.aspx.cs
@@ -13,13 +13,53 @@ namespace ADO_Demo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // SQL Injection 
+
+            // ja' ; delete from hr.candidates; --
+
+            // Query in SQL server Profiler : select * from hr.candidates where fullname like 'ja' ; delete from hr.candidates; --%';
+
+            //string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            //using(SqlConnection conn = new SqlConnection(CS))
+            //{
+            //    string com = "select * from hr.candidates where fullname like '" + TextBox1.Text +"%';";
+            //    conn.Open();
+            //    using(SqlCommand cmd = new SqlCommand(com, conn))
+            //    {
+            //        GridView1.DataSource = cmd.ExecuteReader();
+            //        GridView1.DataBind();
+            //    }
+            //}
+
+            // SQL Prevention using Parameterized Query
+
+            // Query Execute in SQL : exec sp_executesql N'select * from hr.employees where fullname like @fullname',N'@fullname nvarchar(36)',@fullname=N'jo'' ; delete from hr.candidates; --%'
+
+            //treated as a value not as a Query. convert ' -> ''
+
+            //string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            //using (SqlConnection conn = new SqlConnection(CS))
+            //{
+            //    string com = "select * from hr.employees where fullname like @fullname";
+            //    conn.Open();
+            //    using (SqlCommand cmd = new SqlCommand(com, conn))
+            //    {
+            //        cmd.Parameters.AddWithValue("@fullname", TextBox1.Text + "%");
+            //        GridView1.DataSource = cmd.ExecuteReader();
+            //        GridView1.DataBind();
+            //    }
+            //}
+            
+            // SQL Prevention using Stored Procedure
+
             string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
-            using(SqlConnection conn = new SqlConnection(CS))
-            {
-                string com = "select * from hr.candidates where fullname like '" + TextBox1.Text +"%';";
+            using (SqlConnection conn = new SqlConnection(CS))
+            { 
                 conn.Open();
-                using(SqlCommand cmd = new SqlCommand(com, conn))
+                using (SqlCommand cmd = new SqlCommand("spGetFullname", conn))
                 {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@fullname", TextBox1.Text);
                     GridView1.DataSource = cmd.ExecuteReader();
                     GridView1.DataBind();
                 }
@@ -28,4 +68,5 @@ namespace ADO_Demo
     }
 }
 
-// ja' ; delete from hr.candidates; --
+
+
