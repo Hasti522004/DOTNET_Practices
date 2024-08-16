4c183080bfa12ebb33de56740a2d1a255840260d
Hasti522004 <hasti.hajipara@gmail.com>
2024-08-16 10:55:09 +0530

    Types of Syntax : Query,Method and Mixed Syntax

--- LINQDemo/LINQDemo/Program.cs
+++ LINQDemo/LINQDemo/Program.cs
@@ -5,18 +5,38 @@ namespace LINQ_Demo
     {
         public static void Main(string[] args)
         {
+            // LINQ-To-Objects
             int[] age = { 1, 2, 3, 95, 90, 12, 5, 111, 32 };
-            
-            var a = from i in age where i > 20 select i;
+
+            // 1. Query Syntax
+            IEnumerable<int> a = from i in age where i > 20 select i;
             a = from i in age where i > 20 orderby i select i;
             a = from i in age where i > 20 orderby i descending select i;
             foreach (var i in a)
             {
-                Console.WriteLine(i);
+                Console.Write(i + " ");
             }
 
+            Console.WriteLine("---------------------------------------");
+
+            // 2. Method Syntax
+            var methodSyntax = age.Where(i => i > 20);
+            foreach (var i in methodSyntax)
+            {
+                Console.Write(i+" ");
+            }
+
+            Console.WriteLine("---------------------------------------");
+
+            // 3. Mixed Syntax
+            var mixedSyntax = (from i in age
+                               select i).Max();
+
+            Console.WriteLine(mixedSyntax);
+
             string[] names = { "Hasti", "Jatan", "Riya", "Veer", "Palak", "Jay" };
-            var b = from i in names where i.Contains("ha") select i;
+            var b = from i in names where i.Contains("ha") select i; //Not print
+            b = from i in names where i.Contains("Ha") select i; //print
             foreach (var i in b) 
             {
                 Console.WriteLine(i);
