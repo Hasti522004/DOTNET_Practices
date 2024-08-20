7ea73e6c1baa2bc82a9929ac39f0b2490f31d71d
Hasti522004 <hasti.hajipara@gmail.com>
2024-08-20 19:01:03 +0530

    Get data from API by ID

--- Entity_Framework_Demo/Entity_Framework_Demo/Controllers/LanguageController.cs
+++ Entity_Framework_Demo/Entity_Framework_Demo/Controllers/LanguageController.cs
@@ -1,5 +1,6 @@
 ﻿using Entity_Framework_Demo.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Entity_Framework_Demo.Controllers
 {
@@ -15,9 +16,19 @@ namespace Entity_Framework_Demo.Controllers
         }
 
         [HttpGet("")]
-        public ActionResult GetLanguages()
+        //using Asyncronous Programming
+        public async Task<ActionResult> GetLanguages()
         {
-            var result = _appDbContext.Languages.ToList();
+            var result = await _appDbContext.Languages.ToListAsync();
+            // using async
+            var result2 = await (from Language in _appDbContext.Languages select Language).ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLanguagebyIdAsync([FromRoute] int id)
+        {
+            var result = _appDbContext.Languages.FindAsync(id);
             return Ok(result);
         }
     }
