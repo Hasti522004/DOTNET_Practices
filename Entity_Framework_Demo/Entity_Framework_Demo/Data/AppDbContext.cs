f8f254695bfda8545adcba14ab2a985eee4bea47
Hasti522004 <hasti.hajipara@gmail.com>
2024-08-20 15:17:37 +0530

    Create table and Columns using Entity Framework

--- Entity_Framework_Demo/Entity_Framework_Demo/Data/AppDbContext.cs
+++ Entity_Framework_Demo/Entity_Framework_Demo/Data/AppDbContext.cs
@@ -2,7 +2,14 @@
 
 namespace Entity_Framework_Demo.Data
 {
-    public class AppDbContext : DbContext;
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Books> Books { get; set; }
+        public DbSet<Language> Languages { get; set; }
     }
 }
--- Entity_Framework_Demo/Entity_Framework_Demo/Program.cs
+++ Entity_Framework_Demo/Entity_Framework_Demo/Program.cs
@@ -1,4 +1,7 @@
 
+using Entity_Framework_Demo.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Entity_Framework_Demo
 {
     public class Program
@@ -6,7 +9,10 @@ namespace Entity_Framework_Demo
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+           
+            builder.Services.AddDbContext<AppDbContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))); 
+           
             // Add services to the container.
 
             builder.Services.AddControllers();
