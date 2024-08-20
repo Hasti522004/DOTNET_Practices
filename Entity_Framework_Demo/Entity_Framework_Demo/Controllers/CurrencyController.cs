993676bbd0a3caa30adac31ab88c008f26676de4
Hasti522004 <hasti.hajipara@gmail.com>
2024-08-20 18:33:58 +0530

    Get data from databse using web API

---
+++ Entity_Framework_Demo/Entity_Framework_Demo/Controllers/CurrencyController.cs
@@ -0,0 +1,23 @@
+﻿using Entity_Framework_Demo.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Entity_Framework_Demo.Controllers
+{
+    [Route("api/currency")]
+    [ApiController]
+    public class CurrencyController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CurrencyController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        [HttpGet]
+        public ActionResult GetAllCurrency()
+        {
+            var result = _appDbContext.Languages.ToList();
+            return Ok(result);
+        }
+    }
+}
