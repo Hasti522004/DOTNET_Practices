d9d024e85686d5db733e4ce7e5a04f709d6b7581
Hasti522004 <hasti.hajipara@gmail.com>
2024-09-05 18:29:50 +0530

    Web API with Repository Demo for Store data

---
+++ WebAPIDemo/WebAPIDemo/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIDemo.Model;
+
+namespace WebAPIDemo.Controllers
+{
+    [Controller]
+    [Route("api/")]
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return CollegeRepository.students;
+        }
+        [HttpGet("{id}")]
+        public Student GetStudentById(int id)
+        {
+            return CollegeRepository.students.Where(s => s.Id == id).FirstOrDefault();
+        }
+    }
+}
---
+++ WebAPIDemo/WebAPIDemo/Model/CollegeRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace WebAPIDemo.Model
+{
+    public static class CollegeRepository
+    {
+        public static IList<Student> students = new List<Student>()
+        {
+            new Student
+            {
+                Id = 1,
+                Name = "Test 1",
+                Email = "Test 1",
+                Address = "Test 1"
+            },
+            new Student
+            {
+                Id = 2,
+                Name = "Test 2",
+                Email = "Test 2",
+                Address = "Test 2"
+            }
+        };
+    }
+}
