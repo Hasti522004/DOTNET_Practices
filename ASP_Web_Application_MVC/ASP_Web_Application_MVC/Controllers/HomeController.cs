2b77d5a680075bd7275cf8c711135c84335ac411
Hasti522004 <hasti.hajipara@gmail.com>
2024-09-05 11:03:21 +0530

    Action Filter in MVC for log data

--- ASP_Web_Application_MVC/ASP_Web_Application_MVC/Controllers/HomeController.cs
+++ ASP_Web_Application_MVC/ASP_Web_Application_MVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASP_Web_Application_MVC.Filter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,14 @@ using System.Web.Mvc;
 
 namespace ASP_Web_Application_MVC.Controllers
 {
-    [HandleError]
+    [CustomFilter]
+    // [HandleError]
     public class HomeController : Controller
     {
+        [OutputCache(Duration = 100)]
         public ActionResult Index()
         {
-            throw new Exception("This is Unhandle Exception....");
+            // throw new Exception("This is Unhandle Exception....");
             return View();
         }
 
---
+++ ASP_Web_Application_MVC/ASP_Web_Application_MVC/Filter/CustomFilter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ASP_Web_Application_MVC.Filter
+{
+    public class CustomFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Log("OnActionExecuted", filterContext.RouteData,filterContext.Controller);
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Log("OnActionExecuting", filterContext.RouteData,filterContext.Controller);
+        }
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Log("OnResultExecuted", filterContext.RouteData,filterContext.Controller);
+        }
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Log("OnResultExecuting", filterContext.RouteData,filterContext.Controller);
+        }
+        private void Log(string v, RouteData routeData,ControllerBase controller)
+        {
+            var controllerName = routeData.Values["controller"];
+            var methodName = routeData.Values["action"];
+            var message = string.Format("{0}- Controller : {1} action : {2}", methodName, controllerName, v);
+            
+            if(controller is Controller mvccontroller){
+                if(mvccontroller.ViewBag.LogMessage == null)
+                {
+                    mvccontroller.ViewBag.LogMessage = new List<String>();
+                }
+                ((List<string>)mvccontroller.ViewBag.LogMessage).Add(message);
+            }
+
+            Debug.WriteLine(message);
+        }
+    }
+}
