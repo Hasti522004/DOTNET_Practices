14c2dd31a240e3ccbe73998ec47dc6fc86f1ab4d
Hasti522004 <hasti.hajipara@gmail.com>
2024-09-05 15:23:06 +0530

    Fluent Validation in .NET CORE API

---
+++ FluentValidationAPIDemo/FluentValidationAPIDemo/Controllers/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidationAPIDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluentValidationAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        public TaskController()
+        {
+            
+        }
+        [HttpPost]
+        [Route("[Action]")]
+        public TaskItem Post(TaskItem model)
+        {
+            return model;
+        }
+    }
+}
---
+++ FluentValidationAPIDemo/FluentValidationAPIDemo/Models/TaskItem.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+
+namespace FluentValidationAPIDemo.Models
+{
+    public class TaskItem
+    {
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool RemindMe { get; set; }
+        public int? RemindMinutesBeforeDue { get; set; }
+        public List<string> SubItems { get; set;}
+    }
+    public class TaskItemValidator : AbstractValidator<TaskItem>
+    {
+        public TaskItemValidator() 
+        {
+            RuleFor(t => t.Description).NotEmpty();
+            RuleFor(t => t.DueDate).
+                GreaterThanOrEqualTo(DateTime.Today).
+                WithMessage("Due Date must be in the future");
+            When(t => t.RemindMe == true, () =>
+            {
+                RuleFor(t => t.RemindMinutesBeforeDue)
+                .NotNull()
+                .WithMessage("RemindMinutesBeforeDue must be set")
+                .GreaterThan(0)
+                .WithMessage("RemindMinutesBeforeDue must be greater than 0")
+                .Must(value => value % 15 == 0)
+                .WithMessage("RemindMinutesBeforeDue must be multiple of 15");
+            });
+
+            RuleForEach(t => t.SubItems)
+                .NotEmpty()
+                .WithMessage("Value not be Empty");
+        }
+    }
+}
