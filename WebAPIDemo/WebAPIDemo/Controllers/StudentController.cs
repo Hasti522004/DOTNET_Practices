046534e501f4311b81c730eb2f0e695c2aff5f51
Hasti522004 <hasti.hajipara@gmail.com>
2024-09-06 12:08:44 +0530

    web api - CRUD API

--- WebAPIDemo/WebAPIDemo/Controllers/StudentController.cs
+++ WebAPIDemo/WebAPIDemo/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
 using WebAPIDemo.Model;
 
 namespace WebAPIDemo.Controllers
@@ -13,7 +14,7 @@ namespace WebAPIDemo.Controllers
         {
             // using ADO
             var students = new List<StudentDTO>();
-            foreach(var item in CollegeRepository.students)
+            foreach (var item in CollegeRepository.students)
             {
                 StudentDTO obj = new StudentDTO()
                 {
@@ -38,12 +39,12 @@ namespace WebAPIDemo.Controllers
             return Ok(students);
         }
 
-        [HttpGet("{id:int}",Name ="GetStudentById")]
+        [HttpGet("{id:int}", Name = "GetStudentById")]
         [ProducesResponseType(typeof(int), 200)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<Student> GetStudentById(int id)  
+        public ActionResult<Student> GetStudentById(int id)
         {
             // BadRequest - 400 - Client Error
             if (id <= 0)
@@ -61,7 +62,7 @@ namespace WebAPIDemo.Controllers
 
         [HttpGet]
         // string is not use as a routing constrain, in place of that we can use alpha
-        [Route("{name:alpha}",Name = "GetStudentByName")]
+        [Route("{name:alpha}", Name = "GetStudentByName")]
         public Student GetStudentByName(string name)
         {
             return CollegeRepository.students.Where(s => s.Name == name).FirstOrDefault();
@@ -72,7 +73,7 @@ namespace WebAPIDemo.Controllers
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<StudentDTO> CreateStudent([FromBody]StudentDTO model)
+        public ActionResult<StudentDTO> CreateStudent([FromBody] StudentDTO model)
         {
             if (model == null)
                 return BadRequest();
@@ -89,6 +90,85 @@ namespace WebAPIDemo.Controllers
             CollegeRepository.students.Add(st);
             // Create URL/Location for perticular id
             return CreatedAtRoute("GetStudentById", new { id = model.Id }, model);
+        }
+
+        [HttpPut]
+        [Route("UpdateStudent")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult UpdateStudent([FromBody] StudentDTO model)
+        {
+            if (model == null || model.Id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var existingStudent = CollegeRepository.students.Where(s => s.Id == model.Id).FirstOrDefault();
+
+            if (existingStudent == null)
+            {
+                return NotFound();
+            }
+            existingStudent.Name = model.Name;
+            existingStudent.Email = model.Email;
+            existingStudent.Address = model.Address;
+
+            return NoContent();
+        }
+        [HttpPatch]
+        [Route("{id:int}/UpdatePartial")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult UpdateStudentPartial(int id,[FromBody] JsonPatchDocument<StudentDTO> patchDocument)
+        {
+            if (patchDocument == null || id <= 0)
+                return BadRequest();
+
+            var existingStudent = CollegeRepository.students.Where(s => s.Id == id).FirstOrDefault();
+
+            if (existingStudent == null)
+                return NotFound();
+
+            var StudentDTO = new StudentDTO()
+            {
+                Id = existingStudent.Id,
+                Name = existingStudent.Name,
+                Email = existingStudent.Email,
+                Address = existingStudent.Address
+            };
+
+            patchDocument.ApplyTo(StudentDTO,ModelState);
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            existingStudent.Name = StudentDTO.Name;
+            existingStudent.Email = StudentDTO.Email;
+            existingStudent.Address= StudentDTO.Address;
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("DeleteStudent")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<bool> DeleteStudent(int id)
+        {
+            if(id<=0) return BadRequest();
+            var student = CollegeRepository.students.Where(s => s.Id==id).FirstOrDefault();
+            if (student == null)
+                return NotFound($"The Student with Id {id} Not Found.");
+
+            CollegeRepository.students.Remove(student);
+
+            return true;
         }
     }
 }
