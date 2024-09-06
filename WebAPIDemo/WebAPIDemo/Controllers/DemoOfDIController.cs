c01d76f0337da13f64657a642c97e96d4c22e700
Hasti522004 <hasti.hajipara@gmail.com>
2024-09-06 17:15:17 +0530

    web api -Built in Logging in Console and Debug changes

--- WebAPIDemo/WebAPIDemo/Controllers/DemoOfDIController.cs
+++ WebAPIDemo/WebAPIDemo/Controllers/DemoOfDIController.cs
@@ -10,13 +10,19 @@ namespace WebAPIDemo.Controllers
         //1. Strongly coupled/tightly coupled
 
         private readonly IMyLogger _mylogger;
-        public DemoOfDIController(IMyLogger mylogger)
+        private readonly ILogger<DemoOfDIController> _myloggerFactory;
+        public DemoOfDIController(IMyLogger mylogger, ILogger<DemoOfDIController> myloggerFactory)
         {
             _mylogger = mylogger;
+            _myloggerFactory = myloggerFactory;
         }
         [HttpGet]
         public ActionResult Index()
         {
+            _myloggerFactory.LogInformation("From Log Information");
+            _myloggerFactory.LogDebug("From Log Debug");
+            _myloggerFactory.LogError("From Log Error");
+
             _mylogger.Log("Index method Started");
             return Ok();
         }
